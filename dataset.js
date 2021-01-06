758884831ae16f74e1877b451bd51e5b3f536322
moogs <morgan.greff@meetgradient.com>
2021-01-05 21:35:36 -0700

    Homework Assignment

---
+++ dataset.js
@@ -0,0 +1,83 @@
+export const dataset = {
+    characters: [
+        {
+            firstName: 'Spike',
+            lastName: 'Spiegel',
+            personalityTraits: ['slothful', 'nonchalant', 'indifferent', 'lazy'],
+            quotes: [
+                'Spike Spiegel: "Jet, do you know that there are three things that I particularly hate?"\n' +
+                'Jet Black: [defeated] "Really?"\n' +
+                'Spike Spiegel: "Kids. Animals. And women with attitude.',
+                'Whatever happens, happens.',
+                'Bang...'
+            ],
+        },
+        {
+            firstName: 'Jet',
+            lastName: 'Black',
+            personalityTraits: ['mature', 'wise', 'austere', 'patient'],
+            quotes: [
+                'It’s just like Charlie said in my dream. If you want to receive you have to give. See Spike, you got to listen to your dreams, that’s how you find your dream girl.',
+                'Don’t be dense. I started wailing the blues when the doctor whacked my bottom on the day I was born',
+            ],
+        },
+        {
+            firstName: 'Faye',
+            lastName: 'Valentine',
+            personalityTraits: ['arrogant', 'lazy'],
+            quotes: [
+                'Survival of the fittest is the law of nature. We deceive or we are deceived, thus we flourish or perish. Nothing good ever happened to me when I trusted others. That, is the lesson.',
+                'Don\'t you know? I already am a fairy',
+                'You know what they say, cowboy. Easy come, Easy go.',
+                'You know the first rule of combat? Shoot them before they shoot you',
+                'When choosing between two evils. I always like to try the one i never tried before.',
+                'Ordinary. The kind of beautiful, dangerous ordinary that you just can\'t leave alone.',
+                'Men are such idiots',
+                'And you can’t tell what a woman will DO by looking, so you’ll have to be very careful.',
+            ],
+        },
+        {
+            firstName: 'Edward',
+            lastName: 'Tivruski',
+            personalityTraits: ['eccentric', 'resourceful', 'giggly', 'curious', 'intelligent'],
+            quotes: [
+                'Satellite from days of old, lead me to your access code!',
+                'Where are you?! Are you heeerree, Mr. Spooky space creature?!',
+                'High socks are cool! Gotta wear \'em outside!',
+            ],
+        },
+        {
+            firstName: 'Vicious',
+            lastName: '',
+            personalityTraits: ['cold', 'sadistic', 'ruthless', 'bloodthirsty', 'ambitious man'],
+            quotes: [
+                'There is nothing to believe in. There is no need to believe.',
+                'And you will shed tears of scarlet',
+                'I\'m the only one who can keep you alive. And I\'m the only one who can kill you.',
+            ],
+        }
+    ],
+    ships: [
+        {
+            name: 'Hammer Head',
+            model: 'MONO boat',
+            ownerId: 'Jet Black',
+            crewSize: 1,
+            active: true
+        },
+        {
+            name: 'Swordfish II',
+            model: 'MONO racer, Model 2405',
+            ownerId: 'Spike Spiegel',
+            crewSize: 1,
+            active: true
+        },
+        {
+            name: 'Red Tail',
+            model: 'MONO carrier',
+            ownerId: 'Faye Valentine',
+            crewSize: 1,
+            active: true
+        }
+    ]
+};
---
+++ index.js
@@ -0,0 +1,105 @@
+/*
+* There are six questions below. For every question, first do an implementation connecting to the Mongo collection
+* followed by an implementation in native javascript manipulating the dataset object. Mongo documentation has been
+* included in the questions as well.
+*
+* The dataset object is a json representation of the MongoDB database.
+* */
+'use strict'
+import mongoDB from 'mongodb';
+import { dataset } from './dataset.js';
+const { MongoClient } = mongoDB;
+
+async function main() {
+    /**
+     * Connection URI. This should be present in the email that was sent with this assignment.
+     */
+    const uri = "mongodb+srv://tempUser:OUVxpaaTUob9jvMM@mars.9bbf5.mongodb.net/homework?retryWrites=true&w=majority";
+
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html for more details
+     */
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db("homework");
+
+        /*****************************************************************************************************
+         *   Question One                                                                                    *
+         *                                                                                                   *
+         * Write a MongoDB query to display the total number of documents in the collection ships.           *
+         * See https://docs.mongodb.com/manual/reference/method/db.collection.count/ for more details        *
+         ****************************************************************************************************/
+
+        //MongoDB
+        //example: await database.collection("characters").count();
+
+        //Javascript
+        //example: json.characters.length;
+
+        /*****************************************************************************************************
+         *   Question Two                                                                                    *
+         *                                                                                                   *
+         * Write a MongoDB query to display any 5 documents using pretty format in the collection ships.     *
+         * See https://docs.mongodb.com/manual/reference/method/db.collection.find/ for more details.        *
+         ****************************************************************************************************/
+
+        //MongoDB
+
+        //Javascript
+
+        /*****************************************************************************************************
+         *   Question Three                                                                                  *
+         *                                                                                                   *
+         * Write a MongoDB query to display the details of the character with the most quotes.               *
+         * See https://docs.mongodb.com/manual/reference/operator/aggregation/sort/ for more details         *
+         ****************************************************************************************************/
+
+        //MongoDB
+
+        //Javascript
+
+        /*****************************************************************************************************
+         *   Question Four                                                                                   *
+         *                                                                                                   *
+         * Write a MongoDB query to display any characters who have one of these personality traits:         *
+         * “mature” or “lazy” in collection characters.                                                      *
+         * See https://docs.mongodb.com/manual/reference/operator/aggregation/in/ for more details           *
+         ****************************************************************************************************/
+
+        //MongoDB
+
+        //Javascript
+
+        /*****************************************************************************************************
+         *   Question Five                                                                                   *
+         *                                                                                                   *
+         * Write a MongoDB query to display characters with a count of their quotes, and a count of their    *
+         * personality traits.                                                                               *
+         * See https://docs.mongodb.com/manual/reference/operator/aggregation/count/ for more details        *
+         ****************************************************************************************************/
+
+        //MongoDB
+
+        //Javascript
+
+        /*****************************************************************************************************
+         *   Question Six                                                                                    *
+         *                                                                                                   *
+         * Write a MongoDB query to display ships, joining the related character based on the ownerId.       *
+         * See https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/ for more details       *
+         ****************************************************************************************************/
+
+        //MongoDB
+
+        //Javascript
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+main().catch(console.error);
